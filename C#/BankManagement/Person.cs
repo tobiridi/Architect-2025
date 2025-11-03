2bf126fd66264d7ae7915a49d6c80bc8745b1b95
tobiridi <tony.6141@hotmail.com>
2025-11-03 10:02:22 +0100

    C# OOP and advanced

---
+++ C#/BankManagement/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagement
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Person(string lastName, string firstName, DateTime birthDate)
+        {
+            
+        }
+    }
+}
---
+++ C#/Delegue_event_generic/GenericModels/GenericClass.cs
@@ -0,0 +1,18 @@
+﻿namespace Delegue_event_generic.GenericModels
+{
+    public class GenericClass<T> where T : class, new()
+    {
+        public Action<int, string> MyAction { get; set; }
+
+        public GenericClass() {
+            MyAction += delegate (int a, string b) { Console.WriteLine($"params {a} | {b}"); };
+
+            MyAction(10, "toto");
+        }
+    }
+
+    public class GenericClass2<K,V>
+    {
+
+    }
+}
